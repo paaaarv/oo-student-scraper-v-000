31c47f4adf98f20e74a30a263925c816af5b692c
Parv Kumar <parvathikumar92@gmail.com>
2017-12-06 23:05:47 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,7 +31,7 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    students_array.each do |name, location|
+    students_array.each do |name|
       binding.pry
     end
 
