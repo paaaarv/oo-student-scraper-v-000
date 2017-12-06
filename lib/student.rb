5d3ec288b2297211c0ed80d27820596b255ff3e4
Parv Kumar <parvathikumar92@gmail.com>
2017-12-06 23:09:08 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -25,7 +25,7 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-    attributes_hash.each do |attribute| 
+    attributes_hash.each do |attribute|
       binding.pry
     end
 
